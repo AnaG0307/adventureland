5225aa105955fde87ddc0fb4930128a4d0ed22d0
AnaG0307 <ana.genover@gmail.com>
2021-08-01 17:34:00 +0000

    Initial commit to create main game structure by asking player game and create first room

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
-# python code goes here
+print("\nWelcome to Adventureland, the adventure of you life!\n")
+
+player_name = input("Pleaser enter your name: ")
+
+
+def start_room():
+    """
+    Explains how to game works, what are the features
+    """
+    print(f"\nHello {player_name}, you just wake up and do not recognise the room around you. The last thing your remember is that you were at a dinner party with some friends and one of a sudden everything turned blurry. The room is completely empty except for some earplugs in one corner.\n")
+    
+
+
+start_room()
+
