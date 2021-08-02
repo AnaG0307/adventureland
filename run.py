4ba3785fdd6693b16de7188fc86d6f0bdeaa5b49
AnaG0307 <ana.genover@gmail.com>
2021-08-02 18:33:47 +0000

    Add description to the functions for future reference

--- run.py
+++ run.py
@@ -6,12 +6,18 @@ player_name = input(F"Pleaser enter your name: \n")
 def start_room():
     """
     Explains how to game works, what are the features and gets started with the first challenge.
+    Calls next function to keep playing.
     """
     print(f"Hello {player_name}, you just wake up and do not recognise the room around you. The last thing your remember is that you were at a dinner party with some friends and one of a sudden everything turned blurry. The room is completely empty except for some earplugs in one corner.\n")
     ear_plugs()
 
 
 def ear_plugs():
+    """
+    Player can choose to pick the object 'earplugs', if the player chooses 'yes' the item gets 
+    stored in the inventory function for future use.
+    Requests correct answer from player to keep playing.
+    """
     player_answer = input(f"Do you want to pick up the earplugs? y/n\n")
    
     if player_answer == 'y':
@@ -24,7 +30,21 @@ def ear_plugs():
         return ear_plugs()
 
 
+def inventory():
+    objects_collected = {
+
+    }
+    objects_collected.update("earplugs": True)
+    print(objects_collected)
+
+
 def door_choice():
+    """
+    Player can choose to go through red or blue door.
+    Red door gets the player to the Firebird room and keeps playing.
+    Blue door gets the player to the Wolf room and the game ends.
+    Requests correct answer from player to keep playing.
+    """
     door_answer = input(f"\nThere are two doors in front of you, one is red, the other one is blue. You need to choose one, type 'red' or 'blue'.\n")
 
     if door_answer == 'blue':
@@ -38,9 +58,16 @@ def door_choice():
 
 
 def firebird_room():
+    """
+    Player needs to solve the Firebird's riddle
+    """
     print('workout code for fire bird room')
 
+
 def game_over():
+    """
+    Ends the game and asks the player if s/he wants to play again
+    """
     print('GAME OVER')
 
 
@@ -50,5 +77,6 @@ def main():
     """
     start_room()
     door_choice()
+    inventory()
 
 main()
