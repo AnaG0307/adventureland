dc4fe57f80600c79b9cc6d49ce9ed61acaebc54e
AnaG0307 <ana.genover@gmail.com>
2021-08-12 11:56:04 +0000

    Correct typos in the game text

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def door_choice(inventory):
 the other one is blue. You need to choose one, type 'red' or 'blue'.\n").lower()
 
     if door_answer == 'blue':
-        print("\nThere is a hangry wolf in the room, the door has locked at\
+        print("\nThere is a hangry wolf in the room, the door has locked at \
 your back. You die!\n")
         game_over()
     if door_answer == 'red':
@@ -38,18 +38,18 @@ your back. You die!\n")
 
 def firebird_room(inventory):
     """
-    Player needs to solve the Firebird's riddle
+    Room 2: Player needs to solve the Firebird's riddle
     """
-    a = (" 2: In the next room you find a majestuous Firebird at the central \
+    a = ("\nRoom 2: In the next room you find a majestuous Firebird at the central \
 area, blocking your way to the next door. You try to reach the exit very \
 carefully trying not to disturb the bird but she wakes up and open \
 her wings. She looks right into your eyes and telepathically tells you \
-that if you wishto go through the door you will need to solve the \
+that if you wish to go through the door you will need to solve the \
 following riddle:\n")
 
     print(a)
 
-    riddle = input("What belongs to you but is used by everyone?\n").lower()
+    riddle = input("\nWhat belongs to you but is used by everyone?\n").lower()
 
     if riddle == "your name" or riddle == "name":
         print("\nWell done! The Firebird gifts you with a key.\n")
@@ -60,16 +60,15 @@ following riddle:\n")
         return True
 
     else:
-        riddle = input("Incorrect answer, try again!\n").lower
+        riddle = input("\nIncorrect answer, try again!\n").lower
         firebird_room(inventory)
 
 
 def mermaid_room(inventory):
     """
-    Level 3 of the game. Function access game_over() and the inventory
-    dictionary.
+    Room 3: Function access game_over() and the inventory dictionary.
     """
-    print("\nLevel 3: A mermaid is singing beautifully in the room. the next \
+    print("\nRoom 3: A mermaid is singing beautifully in the room. The next \
 door is easily accessible.\n")
     mermaid_choice = input("\nDo you want to listen to her or use \
 the earplugs? Type 'listen' or 'earplugs'\n").lower()
@@ -93,10 +92,10 @@ you got enchanted by the mermaid. You die!\n")
 
 def sorcerer_room(inventory):
     """
-    Level 4 of the game. Relates to previous function mermaid_room() and next
+    Room 4: Relates to previous function mermaid_room() and next
     function garden_room().
     """
-    print("\nLevel 4: A sorcerer is standing in front of you, you will pass if \
+    print("\nRoom 4: A sorcerer is standing in front of you, you will pass if \
 you solve the riddle.\n")
     sorcerer_riddle = input("\nIt is beautiful, colorful and magical.\
 It lives up in the sky but can't fly. What is it?\n").lower()
@@ -110,9 +109,9 @@ It lives up in the sky but can't fly. What is it?\n").lower()
 
 def garden_room(inventory):
     """
-    Level 5 of the game.
+    Room 5 of the game.
     """
-    print("\nLevel 5: You managed to get outside of the building, it seems \
+    print("\nRoom 5: You managed to get outside of the building, it seems \
 you are in a garden in front of a fence with 2 doors.\n")
     garden_choice = input("\nWhich one will you take, the green or the yellow \
 door? Type 'green' or 'yellow'\n").lower()
@@ -126,7 +125,7 @@ door? Type 'green' or 'yellow'\n").lower()
 You die!\n")
         game_over()
     else:
-        print("\nIncorrect answer, type 'green' or ''yellow' only.\n")
+        print("\nIncorrect answer, type 'green' or 'yellow' only.\n")
         garden_room(inventory)
 
 
